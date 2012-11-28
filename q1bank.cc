6fcb29ea25b774cd10f66a93490bda85d9ec5c53
Jerry Sun <j.x.y.sun@gmail.com>
2012-11-28 11:01:25 -0500

    bank comments

--- q1bank.cc
+++ q1bank.cc
@@ -1,6 +1,11 @@
 #include <uC++.h>
 #include "q1bank.h"
 
+/********** Bank ***********
+ * Purpose: constructor for bank
+ * Returns: Bank
+ * Arguments: numStudents - number of students
+ ***************************/
 Bank::Bank( unsigned int numStudents ) {
   this->numStudents = numStudents;
   amounts = new unsigned int[numStudents];
@@ -10,17 +15,35 @@ Bank::Bank( unsigned int numStudents ) {
   noMoney = new uCondition[numStudents];
 }
 
+/********* ~Bank ***********
+ * Purpose: destructor for bank; frees allocated objects
+ * Returns: void
+ * Arguments:
+ ***************************/
 Bank::~Bank() {
   delete [] amounts;
   delete [] noMoney;
 }
 
+/******** deposit **********
+ * Purpose: deposits money from a student's account
+ * Returns: void
+ * Arguments: id - id of student's account
+              amount - amount to deposit
+ ***************************/
 void Bank::deposit( unsigned int id, unsigned int amount ) {
   amounts[id] += amount;
   if (!noMoney[id].empty())
     noMoney[id].signal();
 }
 
+/******** withdraw **********
+ * Purpose: withdraws money from a student's account, blocks on
+            insufficient funds
+ * Returns: void
+ * Arguments: id - id of student's account
+              amount - amount to withdraw
+ ***************************/
 void Bank::withdraw( unsigned int id, unsigned int amount ) {
   while(amounts[id] < amount) {
     noMoney[id].wait();
